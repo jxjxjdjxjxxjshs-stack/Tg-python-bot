783b49b97298f41cebd037cef78ce7195f25b147
jxjxjdjxjxxjshs-stack <jxjxjdjxjxxjshs@gmail.com>
2026-09-15 21:42:09 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ BOT_TOKEN = "8617201086:AAFQqfmLrzcSBmKj-rwPb9eGgCo2qt7ok1U"
 bot = telebot.TeleBot(BOT_TOKEN)
 
 # Новая версия v=85 для сброса кэша
-WEB_APP_URL = "https://jxjxjdjxjxxjshs-stack.github.io/Tg-python-bot/index.html?v=600"
+WEB_APP_URL = "https://jxjxjdjxjxxjshs-stack.github.io/Tg-python-bot/index.html?v=700"
 
 @bot.message_handler(commands=['start'])
 def start(message):
